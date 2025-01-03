287f3bb74ffd6638815b7e22516b128be72460d1
kserhiichuk <serhiichukkate@gmail.com>
2025-01-03 20:21:29 +0200

    feat: add stub ImageService for handling image uploads

---
+++ backend/src/modules/images/image.service.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import config from '~/libs/config/config';
+
+
+class ImageService {
+  async upload(filePath: string, fileName: string): Promise<string> {
+
+    const fileUrl = `https://`;
+    return fileUrl;
+  }
+}
+
+export { ImageService };
