5286d2bec4a0cabfe785c2f12858fbe42cd028b2
kserhiichuk <serhiichukkate@gmail.com>
2025-01-03 20:57:25 +0200

    feat: add constants and enums for API paths, app paths, data status, content types, and HTTP headers

---
+++ frontend/src/common/enums/api/api-path.enum.ts
@@ -0,0 +1,14 @@
+const ApiPath = {
+  API_URL: import.meta.env.VITE_API_PATH || 'http://localhost:3000',
+
+  // Auth routes
+  AUTH: '/auth',
+  AUTHENTICATED_USER: '/authenticated-user',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+
+  // Users routes
+  USERS: '/users',
+} as const;
+
+export { ApiPath };
