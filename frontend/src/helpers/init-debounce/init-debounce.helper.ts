3673fadd6e50efa8a478c1ca89028a92718fa071
kserhiichuk <serhiichukkate@gmail.com>
2025-01-03 21:08:26 +0200

    feat: add utility functions for query string configuration, string formatting, class name validation, and debounce initialization

---
+++ frontend/src/helpers/init-debounce/init-debounce.helper.ts
@@ -0,0 +1,9 @@
+import libraryDebounce from 'debounce';
+
+type FunctionWithArguments<T> = (...arguments_: T[]) => void;
+
+const initDebounce = <T>(callback: FunctionWithArguments<T>, timeout: number): ReturnType<typeof libraryDebounce> => {
+  return libraryDebounce(callback, timeout);
+};
+
+export { initDebounce };
